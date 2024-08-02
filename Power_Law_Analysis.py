bdba54ad8de56ee9dd4020436815b06f74b8f11e
seraermolenko <sera.ermolenko@gmail.com>
2024-08-02 02:22:49 -0700

    added boot strapping to anaylsis

--- Power_Law_Analysis.py
+++ Power_Law_Analysis.py
@@ -2,6 +2,7 @@ import powerlaw
 import numpy as np
 import nibabel as nib
 import pandas as pd
+from sklearn.utils import resample
 
 # function to compute and print p-values
 def compute_power_law_p_val(results):
@@ -18,10 +19,62 @@ def main():
     left_parcellation = pd.read_csv("Left_Right_Dataframes/left_parcellation_count.csv")
     right_parcellation = pd.read_csv("Left_Right_Dataframes/right_parcellation_count.csv")
  
-    left_data = left_parcellation.drop("Unnamed: 0")
-    right_data = right_parcellation.drop("Unnamed: 0")
-    print(left_data.head())
-    print(right_data.head())
+    left_data = left_parcellation["count"]
+    right_data = right_parcellation["count"]
+    print(left_data)
+    print(right_data)
+
+
+    # fitting data to power law with MLE for alpha paramater and KS for x-min
+    left_results = powerlaw.Fit(left_data)
+    right_results = powerlaw.Fit(right_data)
+
+    # KS test
+    D_Left = left_results.power_law.KS()   
+    D_right = right_results.power_law.KS()   
+
+    # bootstrapping 
+    bootstrap_num = 100
+
+    # bootstrap left results
+    alpha_bootstrap_left = []
+    xmin_bootstrap_left = []
+    ks_statistics_left = []
+
+    for _ in range(bootstrap_num):
+        sample = resample(left_data, replace=True)               # bootstrap sample 
+        
+        left_boot_results = powerlaw.Fit(sample)
+        
+        alpha_bootstrap_left.append(left_boot_results.power_law.alpha)
+        xmin_bootstrap_left.append(left_boot_results.power_law.xmin)
+
+        # KS test 
+        D_left_boot = left_boot_results.power_law.KS()
+        ks_statistics_left.append(D_left_boot)
+
+    # bootstrap right results
+        alpha_bootstrap_right = []
+        xmin_bootstrap_right = []
+        ks_statistics_right = []
+
+        for _ in range(bootstrap_num):
+            sample = resample(right_data, replace=True)               # bootstrap sample 
+            
+            right_boot_results = powerlaw.Fit(sample)
+            
+            alpha_bootstrap_right.append(right_boot_results.power_law.alpha)
+            xmin_bootstrap_right.append(right_boot_results.power_law.xmin)
+
+            # KS test 
+            D_right_boot = right_boot_results.power_law.KS()
+            ks_statistics_right.append(D_right_boot)
+
+
+
+
+
+
 
 
 
