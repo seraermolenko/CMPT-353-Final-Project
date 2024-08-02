aacdabea72562c7ff6593feaabb7595ea1c8868f
seraermolenko <sera.ermolenko@gmail.com>
2024-08-01 21:27:24 -0700

    improved statistical files

--- Inferential_Tests/Log_Likelihood.py
+++ Inferential_Tests/Log_Likelihood.py
@@ -5,15 +5,19 @@ import matplotlib.pyplot as plt
 
 # ideas in this code were learned form https://towardsdatascience.com/detecting-power-laws-in-real-world-data-with-python-b464190fade6
 
+
 # function explained at the bottom 
 def compute_power_law_p_val(results):
-
 
     distribution_list = ['lognormal', 'exponential', 'truncated_power_law', 'stretched_exponential', 'lognormal_positive']
 
     for distribution in distribution_list:
         R, p = results.distribution_compare('power_law', distribution)
-        print("power law vs " + distribution + ": R= " + str(np.round(R, 3)) + ", p= " + str (np.round(p, 3) ))
+        print("\n")
+        print("power law vs ", distribution)
+        print("R = ", np.round(R, 3))
+        print("p =", np.round(p, 3))
+ 
 
     # this p value quantifies the significance level of the likleyhood ratio
     # R is the likley hood ratio, posititve is power law is better fit 
@@ -52,57 +56,82 @@ s_lognormal = np.random.lognormal(m, s, len(x)) * s * np.sqrt(2*np.pi)      # ge
 results_pareto = powerlaw.Fit(s_pareto)
 
 # printing results
-print("alpha = " + str(results_pareto.power_law.alpha))            # estimated tail index
-print("x_min= " + str (results_pareto.power_law.xmin) )           # estimated x min value 
-print('p= ' + str(compute_power_law_p_val(results_pareto)))        # p is a quility score of the pwer law fit is, between 0 and 1, the clsoer to 1, the better the fit 
+print("\n\n")
+print("----------------------------------------")
+print("\n")
+print("Log Likihood using Power Law distributed data, small data set")
+print("\n")
 
-# Calculating best minimal value for power law fit  
-# note that the python packages alpha libary is = to the standard alpha library +1 s
-# so we have to subtract 1 from the value generated form the power law fit to make the comparison to the true alpha vlaue 
+print("Power-law parameters for the generated data:")
+print("alpha: ",results_pareto.power_law.alpha)                     # estimated tail index
+print("x_min: ",results_pareto.power_law.xmin)                      # estimated x min value s
+compute_power_law_p_val(results_pareto)                             # use Log-likihood              
+
+ 
+print("\n")
+print("note that the python packages alpha libary is = to the standard alpha library +1")
+print("so we have to subtract 1 from the value generated form the power law fit to make the comparison to the true alpha vlaue") 
 
 # alpha = 2.9331912195958676                
 # x_min = 1.2703447024073973
-[1] # p = 0.999
 
-# alpha_true = results.power_law.alpha - 1
+alpha_true = results_pareto.power_law.alpha - 1
+print("true alpha: ", alpha_true)
 
+print("\n\n")
+print("----------------------------------------")
+print("\n")
+print("Log Likihood using Lognormaly distributed data, small data set")
+print("\n")
 
 # fit power to log normal data
 results_lognormal = powerlaw.Fit(s_lognormal)
 
 # printing results 
-print("alpha = " + str(results_lognormal.power_law.alpha))
-print("x_min = " + str(results_lognormal.power_law.xmin))
-print('p= ' + str(compute_power_law_p_val(results_lognormal)))
+print("\n\n")
+print("Log-normal parameters for the generated data:")
+print("alpha: ", results_lognormal.power_law.alpha)
+print("x_min: ", results_lognormal.power_law.xmin)
+compute_power_law_p_val(results_lognormal)
 
 # Calculating best minimal value for power law fit
 # alpha = 2.5508694755027337
 # x_min = 76574.4701482522
-[1] # p = 0.999
 
 # so the issue here is that we are getting a good quality score/p value on a lognormla distribution (it is not power law)
 # Why is the lognormla distribution described so well by power law? 
 # Because the x min vlaue is far into the tail 
 # Solution: manually fix the x-min value to force the libary to fit all of the data not just the tail which best-fits power law 
 
+
+print("\n\n")
+print("----------------------------------------")
+print("\n")
+print("Log Likihood using Lognormaly distributed data, small data set with maunual lowest x-min")
+print("\n")
 # fixing xmin so that fit must include all data
 results = powerlaw.Fit(s_lognormal, xmin=np.min(s_lognormal))              # manually settong the x-min argument to the smallest value in the data set (uses all values)
-print("alpha = " + str (results.power_law.alpha))
-print("x_min = " + str (results.power_law.xmin))
+
+
+print("alpha: ", results.power_law.alpha)
+print("x_min: ", results.power_law.xmin)
+
 # alpha = 1.3087955873576855
 # x_min = 2201.318351239509
 
 # can generate estimates of the log normal distribution paramters
-print("mu = " + str (results.lognormal.mu) )
-print("sigma = " + str(results.lognormal.sigma))
+print("mu: ", results.lognormal.mu)
+print("sigma: ", results.lognormal.sigma)
+
 # mu = 10.933481999687547
 # sigma = 0.9834599169175509
 
-# You can compare estimates to the ground truth and see that the fit does a good job 
-# however in practice, we dont know what the true values are so we wouldnt be able to tell which is a better fit 
-# just by looking at the paramaters
+print("You can compare the mu and sigma estimates to the ground truth of the distribution and see that the fit does a good job.")
+print("however in practice, we dont know what the true values are so we wouldnt be able to tell which is a better fit by compring paramater values.")
+print("\n")
+print("If we new the true paramters of our empirical distribution, we could simply access the paramters of each fitted distribution to compare!")
+print("The solution?")
+print("We compute likley hood-ratio between the power law fit and a list of other distributions to give us a sense of which distributions best explains the data ")
 
-# solution? 
-# compute likley hood-ratio between the power law fit and a list of other distributions to give us a sense of which distributions best explains the data 
-
-# REFER TO FUNCTION AT THE TOP 
+print("\n") 
+print("The function at the top of this script is the Log Likihood implentation")
--- Inferential_Tests/README.md
+++ Inferential_Tests/README.md
@@ -5,6 +5,13 @@
 This sections speratly demonstrates the KS test, the Log Likelihood test, and both together with bootstrapping. The maximum likelihood test is implicity demonstrated in each file becuase it is used by the Fit function from Power Law package. These 
 scripts demonsstrate these testes using generated power law data for illistrative purposes. 
 
+The python3 KS_test.py introduces the Kolmogorov-Smirnov (KS) test and boot strapping. This uses generated power law distributed
+data so the output should be our ideal situatio. 
+
+The Log_Likelihood.py introduces the Log Likelihood test and explores the importiance of setting a good x-min paramter. This script 
+demonstrates why setting the x-min value to be the lowest value is importiant if our data set was small. However, our anyalsis will be conduded on large data so this method was not chosen in the end. 
+
+
 # Dependencies
 
 Python 3 is required for these scripts. 
