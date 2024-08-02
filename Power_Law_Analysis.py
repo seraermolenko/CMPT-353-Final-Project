18b5a1c3a2b2cfedbc5859ddbee67ddd708ebc84
seraermolenko <sera.ermolenko@gmail.com>
2024-08-02 01:58:40 -0700

    added log likihood to power law anylsis

--- Power_Law_Analysis.py
+++ Power_Law_Analysis.py
@@ -3,6 +3,16 @@ import numpy as np
 import nibabel as nib
 import pandas as pd
 
+# function to compute and print p-values
+def compute_power_law_p_val(results):
+    distribution_list = ['lognormal', 'exponential', 'truncated_power_law', 'stretched_exponential', 'lognormal_positive']
+    for distribution in distribution_list:
+        R, p = results.distribution_compare('power_law', distribution)
+        print("\n")
+        print("power law vs ", distribution)
+        print("R = ", np.round(R, 3))
+        print("p =", np.round(p, 3))
+    return None
 
 def main():
     left_parcellation = pd.read_csv("Left_Right_Dataframes/left_parcellation_count.csv")
@@ -12,6 +22,8 @@ def main():
     right_data = right_parcellation.drop("Unnamed: 0")
     print(left_data.head())
     print(right_data.head())
+
+
 
 
 
